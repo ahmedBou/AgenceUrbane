c86332b0a33457f3e54fe4923e705c887b55f159
ahmed <ahmedboutayeb01@gmail.com>
2020-03-31 14:17:49 +0100

    ajouter et afficher les departements , ajouter et afficher les reclamation

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const path = require('path');
-const Joi = require('joi')
 const express = require("express");
-const hbs = require('hbs')
-
+const hbs = require('hbs');
+const fs = require('fs');
+var bodyParser = require("body-parser");
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
 // *Define paths for express config
 const publicDirectoryPath = path.join(__dirname, './public');
+// console.log(__dirname);
+
+
+
 const viewPath = path.join(__dirname, './templates/views')
-const partialsPath = path.join(__dirname, './templates/partials') 
+const partialsPath = path.join(__dirname, './templates/partials')
 
 // *setup handlebars engine and views location
 // tell express which templating engine im gonna use
@@ -22,75 +31,94 @@ hbs.registerPartials(partialsPath)
 // *Setup Static directory to server
 app.use(express.static(path.join(publicDirectoryPath)))
 
-app.get('', (req, res) =>{
-  res.render('index', {data: departement})
+// HomePage
+app.get('', (req, res) => {
+  res.render('index')
 })
 
-const departement = [{
-    id: 1,
-    name: "Departement Administratif Et Financier"
-  },
-  {
-    id: 2,
-    name: "Departement De la Gestion Urbaine"
-  },
-  {
-    id: 3,
-    name: "Departement Des Affaires Juridiqueset Foncieres"
-  },
-  {
-    id: 4,
-    name: "Departement Des Etudes"
-  }
-];
+// *************************************************************************
+// Department section
+// Read and write data from json
+// get all departement
+var data = fs.readFileSync('departement.json')
+var arrayOfObjects = JSON.parse(data)
+app.get('/departement', (req, res) => {
+  res.render('departement', {isData: arrayOfObjects})
+})
 
-// departement section
-app.get("/departement", (req, res) => {
-  res.render('departement');
-});
+app.post('/departement', (req, res) => {
+  var addDepartement = req.body.txtName;
+  var an_id = 0
+  if (!arrayOfObjects[arrayOfObjects.length - 1].id) {
+    an_id = arrayOfObjects[arrayOfObjects.length - 1].id + 1
+  }
+
+  arrayOfObjects.push({
+    "id": an_id,
+    "name": addDepartement
+  })
+
+  const arrayJson = JSON.stringify(arrayOfObjects, null, 2)
+
+  fs.writeFile('departement.json', arrayJson, 'utf-8', function (err) {
+    if (err) throw err
+    console.log('Done!')
+  })
+
+  res.render('departement', {
+    isData: arrayOfObjects
+  })
+})
+
+// get a single one
 app.get("/departement/:id", (req, res) => {
-  const depart = departement.find(d => d.id === parseInt(req.params.id));
+  const depart = arrayOfObjects.find(d => d.id === parseInt(req.params.id));
+  // if departement doesn't have a value
   if (!depart)
-    res.status(404).send("The Departmeent with the given ID was not found");
-  res.render( 'departement', {data: depart.name});
+    res.status(404).send("The Departement with the given ID was not found");
+  res.render('departement', {
+    datas: depart.name
+  });
 });
+// end of dep section
 
-// services section
-const services = [{
-  id: 1,
-  name: "service1"
-}];
-
-app.get("/services", (req, res) => {
-  res.render('services');
-});
-
-
-app.post("/services", (req, res) => {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
-  const result = Joi.validate(req.body, schema);
-
-  if (result.error) {
-    // 400 Bad Request
-    res.status(400).send(result.error.details[0].message);
-    return;
+// ************************************************************************************
+// Reclamation Section:
+app.get('/message', (req, res) => {
+  res.render('message', {
+    datas: arrayOfObjects
+  })
+})
+app.post('/message', (req, res) => {
+  let data = fs.readFileSync('message.json')
+  let message = req.body.txtMsg;
+  let serviceM = req.body.service;
+  var reclamation = JSON.parse(data);
+  var an_id = 0;
+  if (!reclamation[reclamation.length - 1].id) {
+    an_id = reclamation[reclamation.length - 1].id + 1
   }
-  const service = {
-    id: services.length + 1,
-    name: req.body.name
-  };
-  services.push(service);
-  res.render(service);
-});
+  reclamation.push({
+    'id': an_id,
+    'a_msg': message,
+    'name_departement': serviceM
+  })
+  const messageJson = JSON.stringify(reclamation, null, 2)
+  fs.writeFile('message.json', messageJson, 'utf-8', function (err) {
+    if (err) throw err
+    console.log('Done!')
+  })
+  res.render('message', {
+    reclamation: reclamation,
+    datas: arrayOfObjects
+  })
+})
+// end of rec section
+
 // Page not found
-app.get('*', (res, req) =>{
+app.get('*', (res, req) => {
   req.render("404")
 });
 
 // Port
-var port = process.env.PORT || 3000;
-app.listen(port, () => console.log(` Listening on port ${port}...`));
+app.listen(3000);
